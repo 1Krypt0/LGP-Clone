eaf1ddab660d258bcbcb9d40d36e7c51318c72b2
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-03-30 15:20:00 +0100

    Add baseline map to scene

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,10 @@
-import "./style.css";
+import { Map } from "./map/map";
 
-import * as THREE from "three";
+const main = () => {
+  const container = document.querySelector("#scene") || new Element();
+  const map = new Map(container);
 
-const scene = new THREE.Scene();
+  map.start();
+};
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x8c2d19 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
-
-animate();
+main();
