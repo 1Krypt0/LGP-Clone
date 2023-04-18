9ad7a8d8df9c910e74fc4a20026ae52cd05a5d8a
rodrigotuna <rodrigotuna.andrade@gmail.com>
2023-04-18 19:45:25 +0100

    Add popups

--- src/map/map.ts
+++ src/map/map.ts
@@ -1,4 +1,4 @@
-import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
+import { PerspectiveCamera, Raycaster, Scene, WebGLRenderer, Vector2} from "three";
 import { createCamera } from "./components/camera";
 import { createPlane } from "./components/plane";
 import { createScene } from "./components/scene";
@@ -15,6 +15,8 @@ class Map {
   private renderer: WebGLRenderer;
   private loop: Loop;
   private mapScene : MapScene;
+  private raycaster : Raycaster;
+  private pointer : Vector2;
 
   constructor(container: Element) {
     this.camera = createCamera();
@@ -22,6 +24,10 @@ class Map {
     this.renderer = createRenderer();
     this.loop = new Loop(this.camera, this.scene, this.renderer);
     this.mapScene = new MapScene(this.scene);
+    this.raycaster = new Raycaster();
+    this.pointer = new Vector2();
+
+    console.log(this.pointer);
 
     const controls = createControls(this.camera, this.renderer.domElement);
     this.loop.updatables.push(controls);
@@ -35,6 +41,21 @@ class Map {
     this.scene.add(plane);
 
     const resizer = new Resizer(container, this.camera, this.renderer);
+
+    document.addEventListener( 'pointerdown', (ev) =>{
+      this.pointer.set( ( ev.clientX / window.innerWidth ) * 2 - 1, - ( ev.clientY / window.innerHeight ) * 2 + 1 );
+
+		  this.raycaster.setFromCamera( this.pointer, this.camera );
+
+		  const intersects = this.raycaster.intersectObjects( this.scene.children, false);
+
+      for(const intersect of intersects) {
+        if(intersect.object.onClick){
+          intersect.object.onClick();
+        }
+      }
+
+    } );
   }
 
   render() {
--- src/map/mapscene.ts
+++ src/map/mapscene.ts
@@ -1,12 +1,15 @@
 import { MapLayer } from "./components/layer";
+import { POI } from "./components/poi";
 
 export class MapScene {
     layerList : Array<MapLayer>;
+    poiList : Array<POI>;
     scene : THREE.Scene;
 
     constructor(scene : THREE.Scene){
         this.scene = scene;
         this.layerList = [];
+        this.poiList = [];
     }
 
     parse(data : any){
@@ -16,6 +19,13 @@ export class MapScene {
             const mapLayer = new MapLayer(layer);
             this.layerList.push(mapLayer);
             this.scene.add(mapLayer.getMesh());
+        }
+        
+        const pois = data.poi;
+        for (const poi of pois){
+            const mapPoi = new POI(poi);
+            this.layerList.push(mapPoi);
+            this.scene.add(mapPoi.getMesh());
         }
     }
 }
