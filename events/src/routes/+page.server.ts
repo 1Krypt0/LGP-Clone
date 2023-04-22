660934fd769cc445e679b4b0cc8c545957645167
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-04-22 21:38:44 +0100

    Create API route for getting events. Improve error handling and data storing in event generation

--- events/src/routes/+page.server.ts
+++ events/src/routes/+page.server.ts
@@ -1,36 +1,41 @@
 import { fail } from '@sveltejs/kit';
 import type { Actions } from './$types';
+import prisma from '$lib/prisma';
 
 export const actions = {
 	default: async (event) => {
 		const data = await event.request.formData();
+
 		const name = data.get('name') as string;
 		const description = data.get('description') as string;
 		const price = Number(data.get('price') as string);
-		const date = new Date(data.get('date') as string);
+		let date: string | Date = data.get('date') as string;
 		const coords = data.get('coordinates') as string;
-		// const files = data.getAll('files') as File[];
 
-		if (name === '') {
-			return fail(400, { name, missing: true });
-		}
-
-		if (description === '') {
-			return fail(400, { description, missing: true });
+		if (!name || !description || !coords || !date) {
+			return fail(400, { name, description, coords, date, price, missing: true });
 		}
 
 		if (isNaN(price) || price < 0) {
 			return fail(400, { price, incorrect: true });
 		}
 
+		date = new Date(date);
+
 		if (date.getTime() <= Date.now()) {
 			return fail(400, { date, incorrect: true });
 		}
 
-		if (coords === '') {
-			return fail(400, { coords, missing: true });
-		}
+		await prisma.event.create({
+			data: {
+				name,
+				description,
+				price,
+				date,
+				coordinates: coords
+			}
+		});
 
-		// TODO: Process the files and save them somewhere
+		return { success: true };
 	}
 } satisfies Actions;
